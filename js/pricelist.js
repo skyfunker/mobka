254e070844a8840f52a03216fd825144e5a9310a
Andriy Isayev <andriy.isayev@gmail.com>
2016-03-10 18:30:11 +0200

    pricelist in bootgrid draft

--- js/pricelist.js
+++ js/pricelist.js
@@ -4,7 +4,34 @@
  * and open the template in the editor.
  */
 $(document).ready(function () {
-    $("#data-table-command").bootgrid({
+    $("#grid-pricelist").bootgrid({
+        navigation: 1,
+        rowCount: [10, 25, 50, -1],
+        selection: false,
+        multiSelect: false,
+        sorting: true,
+        multiSort: false,
+        keepSelection: false,
+        caseSensitive: true,
+        ajax: true,
+        url: 'http://cwport.com/mobka/data/pricelist.json',
+        ajaxSettings: {
+          method: "GET",
+          cache: false
+        },
+        requestHandler: function(request) {
+            return request;
+        },
+        responseHandler: function(response) {
+            return response;
+        },
+//        searchSettings: {
+//          delay: 100,
+//          characters: 3
+//        },
+        labels: {
+            noResults: "No records downloaded"
+        },
         css: {
             icon: 'zmdi icon',
             iconColumns: 'zmdi-view-module',
@@ -13,9 +40,25 @@ $(document).ready(function () {
             iconUp: 'zmdi-expand-less'
         },
         formatters: {
+            "inservice": function (column, row) {
+                var html = '<div class="radio cr-alt m-b-20"><label><input type="checkbox" name="inService" value="' 
+                        + row.pricelistId + '" ' 
+                        + (row.inService == "1" ? 'checked' : '') 
+                        + '><i class="input-helper"></i></label></div>';
+                return html;
+            },
+            "inaction": function (column, row) {
+                var html = '<div class="radio cr-alt m-b-20"><label><input type="checkbox" name="inAction" value="' 
+                        + row.pricelistId + '" ' 
+                        + (row.inAction == "1" ? 'checked' : '') 
+                        + '><i class="input-helper"></i></label></div>';
+                return html;
+            },
             "commands": function (column, row) {
-                return "<button type=\"button\" class=\"btn btn-icon btn-info command-edit\" data-row-id=\"" + row.id + "\"><span class=\"zmdi zmdi-edit\"></span></button> " +
-                        "<button type=\"button\" class=\"btn btn-icon btn-info command-delete\" data-row-id=\"" + row.id + "\"><span class=\"zmdi zmdi-delete\"></span></button>";
+                return "<button type=\"button\" class=\"btn btn-icon btn-info command-edit\" data-row-id=\"" 
+                        + row.pricelistId + "\"><span class=\"zmdi zmdi-edit\"></span></button> " 
+                        + "<button type=\"button\" class=\"btn btn-icon btn-info command-delete\" data-row-id=\"" 
+                        + row.pricelistId + "\"><span class=\"zmdi zmdi-delete\"></span></button>";
             }
         }
     });
